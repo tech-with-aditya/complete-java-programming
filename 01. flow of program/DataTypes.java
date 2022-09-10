494868fa8d3d41c61277ea4131b4dd6b5d46b247
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-10 19:11:14 +0530

    Create DataTypes.java

---
+++ 01. flow of program/DataTypes.java
@@ -0,0 +1,10 @@
+public class DataTypes {
+  public static void main(String[] args) {
+    String name = "Aditya Rawat";
+    int rank = 56;
+    char grade = 'A';
+    double percentage = 92.6;
+    boolean hasPassed = true;
+    System.out.println(name + " scored " + percentage + "% securing himself an '" + grade + "' grade and a rank of " + rank);
+  }
+}
