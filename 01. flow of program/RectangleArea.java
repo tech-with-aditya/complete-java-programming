0dbe3079cdf3cfb046db1009e5cf9a436d7519a7
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-10 21:12:52 +0530

    Create RectangleArea.java

---
+++ 01. flow of program/RectangleArea.java
@@ -0,0 +1,8 @@
+public class RectangleArea {
+  public static void main(String[] args) {
+    double l = 24; // length
+    double b = 8;  // breadth
+    double area = l * b;
+    System.out.println("The area of the rectangle is " + area + " sq. units");
+  }
+}
