3bb28d2a4b4176398bfaaab684ab3b8ce5d28d59
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-10 21:08:49 +0530

    Create RectanglePerimeter.java

---
+++ 01. flow of program/RectanglePerimeter.java
@@ -0,0 +1,8 @@
+public class RectanglePerimeter {
+  public static void main(String[] args) {
+    double l = 24; // length
+    double b = 8; // breadth
+    double p = 2 * (l + b); // perimeter
+    System.out.println("The perimeter of the rectangle is " + p + " units");
+  }
+}
