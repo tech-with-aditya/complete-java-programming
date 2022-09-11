2131cc6b7bba4994bb8ee017a63aa9dd438612a8
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-11 19:16:32 +0530

    Create CelsiusFahrenheit.java

---
+++ 01. flow of program/CelsiusFahrenheit.java
@@ -0,0 +1,7 @@
+public class CelsiusFahrenheit {
+  public static void main(String[] args) {
+    double c = 100; // celsius
+    double f = (c * 9 / 5) + 32; // fahrenheit
+    System.out.println(f + " °F");
+  }
+}
