afb083f6e38fb78c8f03db5dbbb3700a9d22adb5
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-11 20:14:53 +0530

    Create SquareRoot.java

---
+++ 01. flow of program/SquareRoot.java
@@ -0,0 +1,7 @@
+public class SquareRoot {
+  public static void main(String[] args) {
+    double n = 289; // number
+    double sr = Math.sqrt(n); // square root
+    System.out.println(sr);
+  }
+}
