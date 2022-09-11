b47fbba899c2d2a1fbbe95239ccb3c8421247bcd
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-11 19:46:35 +0530

    Create TriangleArea.java

---
+++ 01. flow of program/TriangleArea.java
@@ -0,0 +1,8 @@
+public class TriangleArea {
+  public static void main(String[] args) {
+    double b = 16; // base
+    double h = 12; // height
+    double area =  (b * h) / 2;
+    System.out.println("The area of the triangle is " + area + " sq. units");
+  }
+}
