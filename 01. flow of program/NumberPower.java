1d261b9b1710ec48baca52e405b552b76385ec8f
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-11 19:35:10 +0530

    Create NumberPower.java

---
+++ 01. flow of program/NumberPower.java
@@ -0,0 +1,8 @@
+public class NumberPower {
+  public static void main(String[] args) {
+    double b = 2; // base
+    double e = 3; // exponent
+    double res = Math.pow(b, e); // result
+    System.out.println(res);
+  }
+}
