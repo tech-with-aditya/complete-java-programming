506a5724a671e983f1f01ae8c34197764c1dfaf0
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-11 20:21:00 +0530

    Create ETArea.java

---
+++ 01. flow of program/ETArea.java
@@ -0,0 +1,7 @@
+public class ETArea {
+  public static void main(String[] args) {
+    double s = 9; // side
+    double area = ((Math.sqrt(3) / 4) * (Math.pow(s, 2)));
+    System.out.println("The area of the equilateral triangle is " + area + " sq. units");
+  }
+}
