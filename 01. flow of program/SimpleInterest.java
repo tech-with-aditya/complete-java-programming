7020b2803fe593d10b7758b5bb8a9c37eb91422b
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-11 19:21:59 +0530

    Create SimpleInterest.java

---
+++ 01. flow of program/SimpleInterest.java
@@ -0,0 +1,9 @@
+public class SimpleInterest {
+  public static void main(String[] args) {
+    double p = 25000; // principle amount
+    double r = 6; // rate of interest
+    double t = 8; // time
+    double si = (p * r * t) / 100; // simple interest
+    System.out.println(si);
+  }
+}
