de13af4806bb62c1de4e99cdf4dc399a7576df77
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-12 21:55:10 +0530

    Create Sample.java

---
+++ decision making/Sample.java
@@ -0,0 +1,11 @@
+public class Sample {
+  public static void main(String[] args) {
+    int num = 23;
+    if (num > 20) {
+      System.out.println("You entered a number greater than 20");
+    }
+    else {
+      System.out.println("You entered a number smaller than 20");
+    }
+  }
+}
