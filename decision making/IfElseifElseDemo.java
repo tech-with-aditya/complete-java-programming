67ad15d21dd9962d9d3bacc1ae65ed81f0bcf86d
tech-with-aditya <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-12 23:28:49 +0530

    Create IfElseifElseDemo.java

---
+++ decision making/IfElseifElseDemo.java
@@ -0,0 +1,14 @@
+public class IfElseifElseDemo {
+  public static void main(String[] args) {
+    int num = -4;
+    if (num > 0) {
+      System.out.println("You entered a positive number!");
+    }
+    else if (num == 0) {
+      System.out.println("You entered zero!");
+    }
+    else {
+      System.out.println("You entered a negative number!");
+    }
+  }
+}
